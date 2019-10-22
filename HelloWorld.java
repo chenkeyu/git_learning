6ff11ad7debda5f1202501b67ee2e226329c92fa
vicchen <victor.y.chen@nokia-sbell.com>
2019-10-22 10:59:29 +0800

    commit to local_br1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,16 @@
 
 
 public class HelloWorld{
+  
    public static void main(String[] args){
-    
      System.out.println("Hello, Git!");
-    
    }
+
+   public void test() {
+
+   }
+
+   
 
 
 }
